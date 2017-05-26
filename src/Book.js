7f3419ecfb9a602c8f005169305b1840afc14cc7
drainpip <me@shaneis.me>
2017-05-26 15:58:01 -0700

    Set up router

--- src/Book.js
+++ src/Book.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import './Book.css';
+
+import Intro from './book/Intro';
+import Chapter from './book/Chapter';
 
 class Book extends Component {
   render() {
     return (
-      <div className="Book">
-        <h1 className="Book__title">Copy of Kate</h1>
-        <section className="Book__pages">
-          <p>This is an online, serialized, novel. It is open-source, in that you may read it as it's in-progress and view its changes over time. I don't personally recommend that as things will change so much that either a) they will suck horribly until they're good most of the time, and slightly b) something you like might be removed, moved, or who knows what else.</p>
-          <p>Maybe one day this will all be done.</p>
-          <p>- SD</p>
-        </section>
-      </div>
+      <Router>
+        <div className="Book">
+          <Route exact path="/" component={Intro} />
+          <Route path="/book/:chapter/" component={Chapter} />
+        </div>
+      </Router>
     );
   }
 }
