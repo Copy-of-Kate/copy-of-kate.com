fa015f3aa090d04be59706bcccf20b65de7cc41d
drainpip <me@shaneis.me>
2017-05-26 18:05:08 -0700

    Implement chapter

--- src/book/Chapter.js
+++ src/book/Chapter.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Chapter.css';
 
 const Chapter = (props) => {
   return (
     <section>
-      <h1>Chapter {props.match.params.chapter}</h1>
-      <p>Coming Soon</p>
+      <h1 className="Chapter__title">Chapter {props.match.params.chapter}</h1>
+      <article className="Book__pages">
+        <p>Coming Soon</p>
+      </article>
     </section>
   );
 };
