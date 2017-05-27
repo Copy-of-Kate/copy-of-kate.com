192eb0dea8a60f096e2090e672cd65fdc8284793
drainpip <me@shaneis.me>
2017-05-26 19:52:03 -0700

    Create chapter 1

--- src/book/Chapter.js
+++ src/book/Chapter.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
 import './Chapter.css';
+
+import One from './chapters/One';
 
 const Chapter = (props) => {
   return (
     <section>
       <h1 className="Chapter__title">Chapter {props.match.params.chapter}</h1>
-      <article className="Book__pages">
-        <p>Coming Soon</p>
-      </article>
+      <Switch>
+        <Route path="/book/one/" component={One} />
+        <Route render={() => <p>Not a valid chapter...</p>} />
+      </Switch>
     </section>
   );
 };
