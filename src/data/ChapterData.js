5353a1be19f6c01dec163cf39c409e6e134b4ff8
drainpip <me@shaneis.me>
2017-05-28 13:33:52 -0700

    Add progress messages

--- src/data/ChapterData.js
+++ src/data/ChapterData.js
@@ -3,19 +3,25 @@ const ChapterData = [
     "number": "one",
     "title": "Moving Day",
     "slug": "moving-day",
-    "progress": "in_progress"
+    "progress": "in-progress",
+    "progressLabel": "in progress",
+    "progressMessage": "This chapter is currently being written and is subject to extreme change."
   },
   {
     "number": "two",
     "title": "Birth Day",
     "slug": "birth-day",
-    "progress": "coming_soon"
+    "progress": "coming-soon",
+    "progressLabel": "coming soon",
+    "progressMessage": "This chapter will be written immediately after Chapter One goes into a rough draft state."
   },
   {
     "number": "three",
     "title": "My Hole in the Ground",
     "slug": "my-hole-in-the-ground",
-    "progress": "coming_soon"
+    "progress": "coming-soon",
+    "progressLabel": "coming soon",
+    "progressMessage": "This chapter is only in a rough outline form and will be written a bit after Chapter two goes into rough draft."
   }
 ];
 
