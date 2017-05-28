b4607824fab6accbad2e104fd2fc139d82ba1548
drainpip <me@shaneis.me>
2017-05-28 14:55:05 -0700

    Add open source link

--- src/book/Intro.js
+++ src/book/Intro.js
@@ -7,7 +7,7 @@ const Intro = () => {
     <section>
       <BookTitle />
       <article className="Book__pages">
-        <p>This is an online, serialized, novel. It is open-source, in that you may read it as it's in-progress and view its changes over time. I don't personally recommend that as things will change so much that either a) they will suck horribly until they're good most of the time, and slightly b) something you like might be removed, moved, or who knows what else.</p>
+        <p>This is an online, serialized, novel. It is <a href="https://github.com/Copy-of-Kate/book" title="Github Book Project">open-source</a>, in that you may read it as it's in-progress and view its changes over time. I don't personally recommend that as things will change so much that either a) they will suck horribly until they're good most of the time, and slightly b) something you like might be removed, moved, or who knows what else.</p>
         <p>Maybe one day this will all be done.</p>
         <p>- SD</p>
         <h2>Index</h2>
