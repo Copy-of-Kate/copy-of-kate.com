b0aff762860545515508c7dca321d6704dd8cc1e
drainpip <me@shaneis.me>
2017-05-29 22:53:42 -0700

    Set firebase up on book to pass into a store

--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
+import * as firebase from 'firebase';
 import './Book.css';
 
 import Intro from './book/Intro';
@@ -11,19 +12,38 @@ import Chapter from './book/Chapter';
 import TableOfContents from './book/TableOfContents';
 import NotFound from './NotFound';
 
-const Book = () => {
-  return (
-    <Router>
-      <div className="Book">
-        <Switch>
-          <Route exact path="/" component={Intro} />
-          <Route exact path="/book/" component={TableOfContents} />
-          <Route path="/book/:chapter/:title/" component={Chapter} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+class Book extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      chapters: {}
+    };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('chapters');
+    rootRef.on('value', (snapshot) => {
+      this.setState({
+        chapters: snapshot.val()
+      });
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="Book">
+          <Switch>
+            <Route exact path="/" component={Intro} />
+            <Route exact path="/book/" component={TableOfContents} />
+            <Route path="/book/:chapter/:title/" component={Chapter} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default Book;
--- src/book/TableOfContents.js
+++ src/book/TableOfContents.js
@@ -1,48 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import BookTitle from './BookTitle';
-import * as firebase from 'firebase';
+import ChapterData from '../data/ChapterData';
 import ProgressLabel from '../ProgressLabel';
 import './TableOfContents.css';
 
-class TableOfContents extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      chapters: {}
-    };
-  }
-
-  componentDidMount() {
-    const rootRef = firebase.database().ref().child('chapters');
-    rootRef.on('value', (snapshot) => {
-      this.setState({
-        chapters: snapshot.val()
-      });
-    });
-  }
-
-  render() {
-    return (
-      <section>
-        <Route exact path="/book/" component={BookTitle}/>
-        <ol className="Table-of-contents">
-          {
-            this.state.chapters.length && this.state.chapters.map((chapter) => {
-              return (
-                <li key={chapter.slug}><Link to={{
-                  pathname: "/book/" + chapter.number + "/" + chapter.slug,
-                  state: { chapter: chapter }
-                }}>{chapter.title}</Link> <ProgressLabel chapterData={chapter} /></li>
-              );
-            })
-          }
-        </ol>
-      </section>
-    );
-  }
-
-}
+const TableOfContents = () => {
+  return (
+    <section>
+      <Route exact path="/book/" component={BookTitle}/>
+      <ol className="Table-of-contents">
+        {
+          ChapterData.map((chapter) => {
+            return (
+              <li key={chapter.slug}><Link to={{
+                pathname: "/book/" + chapter.number + "/" + chapter.slug,
+                state: { chapter: chapter }
+              }}>{chapter.title}</Link> <ProgressLabel chapterData={chapter} /></li>
+            );
+          })
+        }
+      </ol>
+    </section>
+  );
+};
 
 export default TableOfContents;
