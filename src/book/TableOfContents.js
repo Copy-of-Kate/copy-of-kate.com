695b8fd33f054b4bd1fb49e56b0ff4b8fb995fbe
drainpip <me@shaneis.me>
2017-05-29 22:42:06 -0700

    Set up firebase connection

--- src/book/TableOfContents.js
+++ src/book/TableOfContents.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 import BookTitle from './BookTitle';
-import ChapterData from '../data/ChapterData';
+import * as firebase from 'firebase';
 import ProgressLabel from '../ProgressLabel';
 import './TableOfContents.css';
 
-const TableOfContents = () => {
-  return (
-    <section>
-      <Route exact path="/book/" component={BookTitle}/>
-      <ol className="Table-of-contents">
-        {
-          ChapterData.map((chapter) => {
-            return (
-              <li><Link to={{
-                pathname: "/book/" + chapter.number + "/" + chapter.slug,
-                state: { chapter: chapter }
-              }}>{chapter.title}</Link> <ProgressLabel chapterData={chapter} /></li>
-            );
-          })
-        }
-      </ol>
-    </section>
-  );
-};
+class TableOfContents extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      chapters: {}
+    };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('chapters');
+    rootRef.on('value', (snapshot) => {
+      this.setState({
+        chapters: snapshot.val()
+      });
+    });
+  }
+
+  render() {
+    return (
+      <section>
+        <Route exact path="/book/" component={BookTitle}/>
+        <ol className="Table-of-contents">
+          {
+            this.state.chapters.length && this.state.chapters.map((chapter) => {
+              return (
+                <li key={chapter.slug}><Link to={{
+                  pathname: "/book/" + chapter.number + "/" + chapter.slug,
+                  state: { chapter: chapter }
+                }}>{chapter.title}</Link> <ProgressLabel chapterData={chapter} /></li>
+              );
+            })
+          }
+        </ol>
+      </section>
+    );
+  }
+
+}
 
 export default TableOfContents;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Book from './Book';
+import * as firebase from 'firebase';
 import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css';
 import './index.css';
+
+const config = {
+  apiKey: "AIzaSyDT1NDmgJkHkm5lxm5pZLukI1IjLUO49xU",
+  authDomain: "copy-of-kate.firebaseapp.com",
+  databaseURL: "https://copy-of-kate.firebaseio.com",
+  projectId: "copy-of-kate",
+  storageBucket: "copy-of-kate.appspot.com",
+  messagingSenderId: "1077024914310"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<Book />, document.getElementById('root'));
 registerServiceWorker();
