80d921babb4f8bc30b145d5f0f0c9ec51307a632
drainpip <me@shaneis.me>
2017-10-29 22:35:46 -0700

    Set up favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Copy of Kate`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [`gatsby-plugin-react-helmet`]
+};
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import './index.css'
+import favicon from "../favicon.ico";
+import "./index.css";
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: "rebeccapurple",
+      marginBottom: "1.45rem"
     }}
   >
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: "1.45rem 1.0875rem"
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            color: "white",
+            textDecoration: "none"
           }}
         >
           Gatsby
@@ -32,33 +33,35 @@ const Header = () => (
       </h1>
     </div>
   </div>
-)
+);
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
+    <Helmet>
+      <title>Copy of Kate, an online novel</title>
+      <meta name="description" content="Copy of Kate" />
+      <meta
+        name="keywords"
+        content="Copy of Kate, Science Fiction, Copy of Kate novel"
+      />
+      <link rel="icon" type="image/x-icon" sizes="16x16" href={favicon} />
+    </Helmet>
     <Header />
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        padding: "0px 1.0875rem 1.45rem",
+        paddingTop: 0
       }}
     >
       {children()}
     </div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
